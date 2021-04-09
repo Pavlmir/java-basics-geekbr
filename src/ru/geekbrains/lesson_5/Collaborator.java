5c9f1c3d62726d144513a267861577bac2b4aab5
pavlmir <mirprost@gmail.com>
2021-04-09 18:46:39 +0300

    lesson 5

--- src/ru/geekbrains/lesson_5/Collaborator.java
+++ src/ru/geekbrains/lesson_5/Collaborator.java
@@ -1,5 +1,35 @@
 package ru.geekbrains.lesson_5;
 
 public class Collaborator {
+    private String fullName; // ФИО
+    private String position; // должность
+    private String email; // электронная почта
+    private String phoneNumber; // телефон
+    private int salary; // зарплата
+    private int age; // возраст
+
+    public Collaborator(String fullName, String position, String email,
+                        String phoneNumber, int salary, int age) {
+        this.fullName = fullName;
+        this.position = position;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.salary = salary;
+        this.age = age;
+    }
+
+    public void getInfo() {
+        System.out.printf("Сотрудник: %s,%n" +
+                        "должность: %s,%n" +
+                        "электронная почта: %s,%n" +
+                        "телефон: %s,%n" +
+                        "зарплата: %s,%n" +
+                        "возраст: %s,%n", this.fullName, this.position,
+                this.email, this.phoneNumber, this.salary, this.age);
+    }
+
+    public int getAge() {
+        return this.age;
+    }
 
 }
--- src/ru/geekbrains/lesson_5/Main.java
+++ src/ru/geekbrains/lesson_5/Main.java
@@ -2,7 +2,42 @@ package ru.geekbrains.lesson_5;
 
 public class Main {
     public static void main(String[] args) {
+        Collaborator[] collabArray = new Collaborator[5]; // Вначале объявляем массив объектов
+        collabArray[0] = new Collaborator("Ivanov Ivan",
+                "Engineer",
+                "ivivan@mailbox.com",
+                "892312312",
+                130000, 30); // потом для каждой ячейки массива задаем объект
 
+        collabArray[1] = new Collaborator("Pavel Gon",
+                "Developer",
+                "pavel@mailbox.com",
+                "892312123",
+                177000, 31);
 
+        collabArray[2] = new Collaborator("Alex Pavlov",
+                "Boss",
+                "alex@mailbox.com",
+                "892312124",
+                190000, 45);
+
+        collabArray[3] = new Collaborator("Bogdan Karanchuk",
+                "Engineer",
+                "bogdan@mailbox.com",
+                "892312125",
+                180000, 41);
+
+        collabArray[4] = new Collaborator("Sergey Loev",
+                "Engineer",
+                "sergey@mailbox.com",
+                "892312127",
+                160000, 27);
+
+        for (int i = 0; i < collabArray.length; i++) {
+            if (collabArray[i].getAge() > 40) {
+                collabArray[i].getInfo();
+                System.out.println("");
+            }
+        }
     }
 }
