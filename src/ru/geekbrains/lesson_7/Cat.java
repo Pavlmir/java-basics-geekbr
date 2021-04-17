0ec94a85b0f44f9ad2fcc3af9da9b383dd791bea
pavlmir <mirprost@gmail.com>
2021-04-17 16:33:52 +0300

    lesson_7

--- src/ru/geekbrains/lesson_7/Cat.java
+++ src/ru/geekbrains/lesson_7/Cat.java
@@ -5,28 +5,29 @@ public class Cat {
     private int appetite;
     private boolean hungry = true;
 
-    public Cat(String name, int appetite){
+    public Cat(String name, int appetite) {
         this.name.append(name);
         this.appetite = appetite;
     }
 
-    public void eat(Plate p){
+    public void eat(Plate p) {
         this.hungry = p.decreaseFood(appetite);
     }
-    public void printinfo(){
-        if(hungry){
+
+    public void printinfo() {
+        if (hungry) {
             System.out.printf("%s голоден %n", name);
-        }
-        else {
+        } else {
             System.out.printf("%s сыт %n", name);
         }
     }
 
-    public void allInfo(){
+    public void allInfo() {
         System.out.printf("%s %s %n", name, appetite);
         printinfo();
     }
-    public boolean getHungry(){
+
+    public boolean getHungry() {
         return this.hungry;
     }
 }
--- src/ru/geekbrains/lesson_7/Plate.java
+++ src/ru/geekbrains/lesson_7/Plate.java
@@ -3,26 +3,25 @@ package ru.geekbrains.lesson_7;
 public class Plate {
     private int food;
 
-    public Plate(int food){
+    public Plate(int food) {
         this.food = food;
     }
 
-    public void info(){
+    public void info() {
         System.out.printf("тарелка %s %n", food);
     }
 
-    public boolean decreaseFood(int n){
-        if(n>food){
+    public boolean decreaseFood(int n) {
+        if (n > food) {
             System.out.println("Нельзя скушать больше, чем есть в миске");
             return true;
-        }
-        else{
+        } else {
             food -= n;
             return false;
         }
     }
 
-    public void putFood(int amountFood){
+    public void putFood(int amountFood) {
         food += amountFood;
     }
 }
