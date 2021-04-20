21e5f6bc004fdf9c88e8252fdd1c57621092196e
pavlmir <mirprost@gmail.com>
2021-04-20 23:32:22 +0300

    lesson 8

---
+++ src/ru/geekbrains/lesson_8/CalcActionListener.java
@@ -0,0 +1,30 @@
+package ru.geekbrains.lesson_8;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class CalcActionListener implements ActionListener {
+    private final JTextField inputField;
+
+    public CalcActionListener(JTextField inputField) {
+        this.inputField = inputField;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        String text = inputField.getText();
+        String[] splittedValues;
+        int sum = 0;
+
+        if (text.contains("+")) {
+            splittedValues = text.split("\\+");
+            sum = Integer.parseInt(splittedValues[0]) + Integer.parseInt(splittedValues[1]);
+        } else if (text.contains("-")) {
+            splittedValues = text.split("-");
+            sum = Integer.parseInt(splittedValues[0]) - Integer.parseInt(splittedValues[1]);
+        }
+
+        inputField.setText(String.valueOf(sum));
+    }
+}
